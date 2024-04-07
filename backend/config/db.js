ca6e46098b26215375ff63498c4f674f34203e60
Aamir793 <amiraftab1997@gmail.com>
2024-04-08 00:04:24 +0530

    process env push

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,21 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri = "mongodb+srv://jeomarkoo1:mongodb@cluster0.6yurqut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 export async function execute(callback) {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("mongodb connection established!");
     return await callback(client);
+  } catch (error) {
+    console.log(error);
   } finally {
     await client.close();
   }
